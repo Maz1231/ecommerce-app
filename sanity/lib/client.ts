4ccbb2c463dcdb55c83dda0fc3f5936e473e7be9
Mazin Idris <mazin1231@hotmail.com>
2023-12-01 12:24:27 -0700

    h

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
+  token: "skJdvTV8hrXZmMTkT9sNrMQMY13Z3erc55Biy74ReinaCABDGChDFXy4IFr9tLxRsm8ONZ1rxx0bKaCESKto1QW7iFKFVqEagLTNVjCtaIPbUAx3YPzkzg7nXBqqyiPQKG3H53WwyzEX9BaUsi5TSb915ZgTvxPkghv0Yt7NIAmW7ZaUK6yY"
 })
