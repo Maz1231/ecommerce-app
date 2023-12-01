638abaab38be308f8a9212525d41bfdf68c970e8
Mazin Idris <mazin1231@hotmail.com>
2023-12-01 10:28:25 -0700

    MZMZ

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Name",
-  description: "Description",
+  name: "MzMz",
+  description: "Expertly designed goods for the workspace, home, travel",
   footer: [
     { name: "Home", href: "/" },
     { name: "About", href: "#" },
