f146768dbe22df0e92a033ebf4e7d21ece586cf8
Mazin Idris <mazin1231@hotmail.com>
2023-12-01 13:32:09 -0700

    Fliter

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,34 @@ import { ProductSort } from "@/components/product-sort"
 import { seedSanityData } from "@/lib/seed"
 import { product } from "@/sanity/schemas/product-schema"
 
-interface Props {}
+interface Props {
+  searchParams: {
+    date?: string
+    price?: string
+    color?: string
+    category?: string
+    size?: string
+  }
+}
 
-export default async function Page() {
+export default async function Page({ searchParams }: Props) {
+  const { date = "desc", price, color, category, size} = searchParams
+  const priceOrder= price
+   ? ` | order(price ${price})`
+    : ""
+  const dateOrder = date
+   ? ` | order(_createdAt ${date})` 
+   : ""
+  const order =  `${priceOrder}${dateOrder}`
 
- const products = await client.fetch<SanityProduct[]>(
-   groq`*[_type == "product"] {
+  const productFilter = `_type == "product"`
+  const colorFilter = color ? `&& "${color}" in colors` : ""
+  const categoryFilter = category ? `&& "${category}" in categories` : ""
+  const sizeFilter = size ? `&& "${size}" in sizes` : ""
+  const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}]`
+
+  const products = await client.fetch<SanityProduct[]>(
+   groq`${filter} ${order} {
    _id, 
    _createdAt,
    name,
